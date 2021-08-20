101538f005b9806d6cddcc908281ce3b63908fd4
Guilherme Augusto Sbizero Correa <68513456+GuilhermeSbizero0804@users.noreply.github.com>
2021-08-20 08:35:25 -0300

    Add files via upload

---
+++ aula02/prj01/src/Pessoa.java
@@ -0,0 +1,18 @@
+public class Pessoa {
+    public static void main(String[] args) {
+        String nome;
+        int idade;
+        double salario;
+        double reajuste;
+        
+        nome = "Guilherme";
+        idade = 19;
+        salario = 600.00;
+        reajuste = salario * 0.15;
+        
+        System.out.println("Nome: .........." + nome);
+        System.out.println("Idade: ........." + idade);
+        System.out.println("Salario: ......." + salario);
+        System.out.println("Reajuste: ......." + reajuste);
+    }
+}
